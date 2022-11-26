30456ec682f39799dbcc2896362d26c6fb9887ee
Abu Talha <abutalha141999@gmail.com>
2022-11-26 18:11:32 +0600

    firebase secret code secuired

--- src/firebase/firebaseAuthConfig.js
+++ src/firebase/firebaseAuthConfig.js
@@ -1,18 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBP8lW9L7WvEXLGQLk26uWSCFtl0tDqAh0",
-  authDomain: "resellerbd-b4e3e.firebaseapp.com",
-  projectId: "resellerbd-b4e3e",
-  storageBucket: "resellerbd-b4e3e.appspot.com",
-  messagingSenderId: "499720770846",
-  appId: "1:499720770846:web:2fcc3cf8b4a3e28a47650f"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app
